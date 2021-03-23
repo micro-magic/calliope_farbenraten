b0ed9156d622a19ab1ee30eda0c040504b12c188
micro-magic <81254371+micro-magic@users.noreply.github.com>
2021-03-23 18:11:30 +0100

    Erste Dateien für MakeCode-Projekt

---
+++ README.md
@@ -0,0 +1,64 @@
+
+> Diese Seite bei [https://micro-magic.github.io/calliope_mini_farbenraten/](https://micro-magic.github.io/calliope_mini_farbenraten/) öffnen
+
+## Als Erweiterung verwenden
+
+Dieses Repository kann als **Erweiterung** in MakeCode hinzugefügt werden.
+
+* öffne [https://makecode.calliope.cc/](https://makecode.calliope.cc/)
+* klicke auf **Neues Projekt**
+* klicke auf **Erweiterungen** unter dem Zahnrad-Menü
+* nach **https://github.com/micro-magic/calliope_mini_farbenraten** suchen und importieren
+
+## Dieses Projekt bearbeiten ![Build Status Abzeichen](https://github.com/micro-magic/calliope_mini_farbenraten/workflows/MakeCode/badge.svg)
+
+Um dieses Repository in MakeCode zu bearbeiten.
+
+* öffne [https://makecode.calliope.cc/](https://makecode.calliope.cc/)
+* klicke auf **Importieren** und dann auf **Importiere URL**
+* füge **https://github.com/micro-magic/calliope_mini_farbenraten** ein und klicke auf Importieren
+
+## Blockvorschau
+
+Dieses Bild zeigt den Blockcode vom letzten Commit im Master an.
+Die Aktualisierung dieses Bildes kann einige Minuten dauern.
+
+![Eine gerenderte Ansicht der Blöcke](https://github.com/micro-magic/calliope_mini_farbenraten/raw/master/.github/makecode/blocks.png)
+
+#### Metadaten (verwendet für Suche, Rendering)
+
+* for PXT/calliopemini
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
+
+
+
+> Diese Seite bei [https://micro-magic.github.io/calliope_farbenraten/](https://micro-magic.github.io/calliope_farbenraten/) öffnen
+
+## Als Erweiterung verwenden
+
+Dieses Repository kann als **Erweiterung** in MakeCode hinzugefügt werden.
+
+* öffne [https://makecode.calliope.cc/](https://makecode.calliope.cc/)
+* klicke auf **Neues Projekt**
+* klicke auf **Erweiterungen** unter dem Zahnrad-Menü
+* nach **https://github.com/micro-magic/calliope_farbenraten** suchen und importieren
+
+## Dieses Projekt bearbeiten ![Build Status Abzeichen](https://github.com/micro-magic/calliope_farbenraten/workflows/MakeCode/badge.svg)
+
+Um dieses Repository in MakeCode zu bearbeiten.
+
+* öffne [https://makecode.calliope.cc/](https://makecode.calliope.cc/)
+* klicke auf **Importieren** und dann auf **Importiere URL**
+* füge **https://github.com/micro-magic/calliope_farbenraten** ein und klicke auf Importieren
+
+## Blockvorschau
+
+Dieses Bild zeigt den Blockcode vom letzten Commit im Master an.
+Die Aktualisierung dieses Bildes kann einige Minuten dauern.
+
+![Eine gerenderte Ansicht der Blöcke](https://github.com/micro-magic/calliope_farbenraten/raw/master/.github/makecode/blocks.png)
+
+#### Metadaten (verwendet für Suche, Rendering)
+
+* for PXT/calliopemini
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ main.ts
@@ -0,0 +1,101 @@
+input.onPinPressed(TouchPin.P0, function () {
+    antwortliste.push(1)
+    basic.setLedColor(0xff0000)
+    basic.pause(1000)
+    basic.turnRgbLedOff()
+})
+input.onPinPressed(TouchPin.P3, function () {
+    antwortliste.push(4)
+    basic.setLedColor(0xffff00)
+    basic.pause(1000)
+    basic.turnRgbLedOff()
+})
+function pruefe_Eingabe () {
+    for (let Index = 0; Index <= level; Index++) {
+        if (farbliste[Index] != antwortliste[Index]) {
+            return false
+        }
+    }
+    return true
+}
+/**
+ * Die Farben werden nacheinander mit der RGB-LED angezeigt.
+ */
+input.onButtonPressed(Button.A, function () {
+    for (let Index = 0; Index <= level; Index++) {
+        if (farbliste[Index] == 1) {
+            basic.setLedColor(0xff0000)
+        } else if (farbliste[Index] == 2) {
+            basic.setLedColor(0x0000ff)
+        } else if (farbliste[Index] == 3) {
+            basic.setLedColor(0x00ff00)
+        } else if (farbliste[Index] == 4) {
+            basic.setLedColor(0xffff00)
+        }
+        basic.pause(1000)
+        basic.turnRgbLedOff()
+        basic.pause(1000)
+    }
+    basic.turnRgbLedOff()
+})
+input.onPinPressed(TouchPin.P2, function () {
+    antwortliste.push(3)
+    basic.setLedColor(0x00ff00)
+    basic.pause(1000)
+    basic.turnRgbLedOff()
+})
+/**
+ * Pin 0 - Pin 3:  Antwort-Farbkombination eingeben
+ */
+input.onPinPressed(TouchPin.P1, function () {
+    antwortliste.push(2)
+    basic.setLedColor(0x0000ff)
+    basic.pause(1000)
+    basic.turnRgbLedOff()
+})
+/**
+ * Spiel "Farbenraten": Startbedingungen
+ * 
+ * maximal 8 Level = Die Farben werden nacheinander angezeigt. Level 1 = eine Farbe bis Level 8 = acht Farben
+ * 
+ * (rot = 1 -> Pin 0, blau = 2 -> Pin 1, grün = 3 -> Pin 2, gelb = 4 -> Pin 3)
+ * 
+ * Die Farben werden über die Zahlen zufällig bestimmt und in einem Feld (engl.: array) 'farbliste' gespeichert.
+ */
+let antwortliste: number[] = []
+let farbliste: number[] = []
+let level = 0
+level = 0
+let alles_richtig = true
+farbliste = []
+for (let index = 0; index < 8; index++) {
+    farbliste.push(randint(1, 4))
+}
+/**
+ * Spielablauf:
+ * 
+ * Wenn Level = Anzahl eingegebener Farben ist, dann wird die Farbliste mit der Antwortliste Element für Element verglichen (-> Funktionsaufruf: 'prufe_Eingabe').
+ * 
+ * Wenn alles richtig war, wird der Level erhöht und die Antwortliste gelöscht. Wenn ein Fehler gemacht wurde, wird das Spiel beendet.
+ */
+basic.forever(function () {
+    if (level + 1 == antwortliste.length) {
+        if (pruefe_Eingabe()) {
+            basic.showIcon(IconNames.Yes)
+            level += 1
+            if (level > 7) {
+                basic.showString("Spiel gewonnen!")
+                control.reset()
+            }
+            antwortliste = []
+            basic.pause(1000)
+            basic.showString("" + (level + 1))
+            basic.pause(1000)
+            basic.clearScreen()
+        } else {
+            basic.showIcon(IconNames.No)
+            basic.showString("verloren Spielende!")
+            control.reset()
+        }
+    }
+})
